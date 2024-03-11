41d58cebac824b4b9d4d9de41383ef6139499075
TobiLight <jgetitdonefast@gmail.com>
2024-03-11 20:43:29 +0000

    ✔ feat: Class representing a DB.

---
+++ utils/db.js
@@ -0,0 +1,59 @@
+/**
+ * Class representing a DB.
+ * Author: Oluwatobiloba Light
+ * File: redis.js
+ */
+
+import { MongoClient } from 'mongodb'
+
+class DBClient {
+  /**
+   * Creates a new DB client instance.
+   */
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`
+    this.client = new MongoClient(url, { useUnifiedTopology: true })
+    this.clientConnected = false;
+
+    this.client.connect().then(() => {
+      this.clientConnected = true;
+    }).catch((err) => console.log(err.message || err.toString()));
+  }
+
+  /**
+   * Checks whether connection to MongoDB is succesful
+   * 
+   * @returns {Boolean} true if connected, otherwise false.
+   */
+  isAlive() {
+    return this.clientConnected;
+  }
+
+  /**
+   * Asynchronously retrieves the number of documents in the "users" collection.
+   * 
+   * @returns {Promise<number>} A promise that resolves to the number of
+   * documents (integer).
+   */
+  async nbUsers() {
+    await this.client.connect();
+    return this.client.db(this.database).collection('users').countDocuments();
+  }
+
+  /**
+   * Asynchronously retrieves the number of documents in the "files" collection.
+   * 
+   * @returns {Promise<number>} A promise that resolves to the number of
+   * documents (integer).
+   */
+  async nbFiles() {
+    await this.client.connect();
+    return this.client.db(this.database).collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient;
+module.exports = dbClient;
