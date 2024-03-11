e271b1833a94d4a3de96c2c7b6ecd4a5876ee23c
TobiLight <jgetitdonefast@gmail.com>
2024-03-11 22:02:27 +0000

    ✔ feat: Setup express server with routes

--- server.js
+++ server.js
@@ -5,14 +5,14 @@
  */
 
 import express from 'express';
-import { router as AppRouter } from "./routes/index";
+import { router as AppRouter } from './routes/index';
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(AppRouter)
+app.use(express.urlencoded({ extended: false }));
+app.use(AppRouter);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
