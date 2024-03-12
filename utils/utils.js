952790b3d105c49fe1a1253ce1665713d35943b3
TobiLight <jgetitdonefast@gmail.com>
2024-03-12 19:39:08 +0000

    ✔ fix: code lint, fix, documentation

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,11 @@
 import sha1 from 'sha1';
 
+/**
+ * Hashes a password
+ * 
+ * @param {String} password - The password to be hashed
+ * @returns {String} - The hashed value of the password
+ */
 export const hashPassword = (password) => sha1(password);
 
 export const getToken = () => { };
